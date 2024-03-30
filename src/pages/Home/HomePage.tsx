e8eca67093a88901259ec85770f28319df4ea6bf
naffiziqbal <f.nafiziqbal@gmail.com>
2024-03-30 15:37:15 +0600

    Form Event

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,30 +1,54 @@
 import { useAppDispatch, useAppSelector } from "../../redux/hooks/hooks";
 import { completed, remove } from "../../redux/todo/todoSlice";
 import image from "../../assets/convertible car-bro.svg";
+import { BsTrash2Fill } from "react-icons/bs";
 
 const Home = () => {
   const dispatch = useAppDispatch();
   const { todo_list } = useAppSelector((state) => state.todoSlice);
 
   return (
-    <div className="w-full justify-center flex flex-col items-center h-full">
-      {todo_list.length ? (
-        todo_list.map((data) => (
-          <div className="flex gap-3">
-            <p onClick={() => dispatch(completed(data))}>{data.name}</p>{" "}
-            <span onClick={() => dispatch(remove(data))}>X</span>
-          </div>
-        ))
-      ) : (
-        <div>
-          <img className="w-full object-cover" src={image} alt="No Todo" />
-          <h3 className="text-2xl font-bold my-6">
-            <span className="text-emerald-400">Well Done!</span> You Have No
-            more Task Today
-          </h3>
-        </div>
+    <>
+      {todo_list.length > 0 && (
+        <h3 className="mt-5 text-2xl font-bold border-b-2 border-b-emerald-500 w-fit mx-3">
+          {" "}
+          Today's Tasks
+        </h3>
       )}
-    </div>
+      <div className="w-full  flex flex-col h-fit px-4 gap-5 rounded-md mt-12">
+        {todo_list.length ? (
+          todo_list.map((data, idx) => (
+            <ul className="flex border-b cursor-pointer gap-3 min-h-12 items-center  px-2">
+              <li className="*:mr-2" onClick={() => dispatch(completed(data))}>
+                <span>{(idx += 1)}.</span>
+                <span>{data.name}</span>
+              </li>{" "}
+              <span
+                className="absolute right-10"
+                onClick={() => dispatch(remove(data))}
+              >
+                <BsTrash2Fill className="text-red-500 h-6 w-6 cursor-pointer" />
+              </span>
+            </ul>
+          ))
+        ) : (
+          <div
+            className="flex flex-col
+           justify-center items-center"
+          >
+            <img
+              className="w-full  max-w-2xl object-cover"
+              src={image}
+              alt="No Todo"
+            />
+            <h3 className="text-2xl font-bold my-6">
+              <span className="text-emerald-400">Well Done!</span> You Have No
+              more Task Today
+            </h3>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/Todos/AddNew.tsx
+++ src/pages/Todos/AddNew.tsx
@@ -1,13 +1,14 @@
 import { useAppDispatch } from "../../redux/hooks/hooks";
 import { addNew } from "../../redux/todo/todoSlice";
 import image from "../../assets/Checklist-bro.svg";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 const AddNew = () => {
   const dispatch = useAppDispatch();
   const [text, setText] = useState("");
 
-  const handleOnclick = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     if (text.length <= 0) {
       alert("Please Add Something");
       return;
@@ -32,7 +33,7 @@ const AddNew = () => {
           <p>Add new Task to manage your day! Never Procrastinate</p>
         </section>
       </section>
-      <section className="w-full md:w-1/2 flex">
+      <form onSubmit={handleSubmit} className="w-full md:w-1/2 flex">
         <input
           type="text"
           onChange={handleText}
@@ -40,13 +41,12 @@ const AddNew = () => {
           placeholder="What's in your mind?"
           className=" w-full rounded-s px-3 ring ring-emerald-500  outline-none"
         />
-        <button
-          onClick={handleOnclick}
+        <input
+          type="submit"
+          value={"Add Now"}
           className="px-3 rounded-e-md  h-fit text-nowrap ring ring-emerald-500 bg-emerald-500 hover:bg-white hover:text-emerald-500 text-white font-bold duration-300"
-        >
-          Add Now
-        </button>
-      </section>
+        />
+      </form>
     </div>
   );
 };
